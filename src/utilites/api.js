a0e6e3b32fa3d2326a1c3b179794a08297105a23
Maksym-Plutenko <plutenkom@gmail.com>
2023-03-04 20:40:27 +0200

    get request refactoring

--- src/utilites/api.js
+++ src/utilites/api.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
 const apiHandler = {
-    key: 'key=33025300-4f56a11ea42b0ad7a58370454',
+  key: 'key=33025300-4f56a11ea42b0ad7a58370454',
 
-    async findPictures(request = '', page = 1) {
-        const response = await axios.get(`https://pixabay.com/api/?q=${request}&page=${page}&${this.key}&image_type=photo&orientation=horizontal&per_page=12`);
-        // console.log(request);
-        // console.log(page);
-        // console.log(response);
-        return response;
-    }
-}
+  async findPictures(request = '', page = 1) {
+    axios.defaults.baseURL = "https://pixabay.com/api";
+    const response = await axios.get(
+      `?q=${request}&page=${page}&${this.key}&image_type=photo&orientation=horizontal&per_page=12`
+    );
+    return response;
+  },
+};
 
-export {apiHandler};
+export { apiHandler };
